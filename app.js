1bf729c2ceedc8ada4943b996f919503f0f7d97e
RebeccaHA <rebeccahunt034@gmail.com>
2020-12-29 20:17:17 +0000

    added current round property to use to limit special attack

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = Vue.createApp({
   data() {
     return {
       monsterHealth: 100,
-      playerHealth: 100
+      playerHealth: 100,
+      currentRound: 0
     };
   },
   computed: {
@@ -22,16 +23,18 @@ const app = Vue.createApp({
       const attackValue = randomNumber(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
+      this.currentRound += 1;
     },
     attackPlayer() {
       const attackValue = randomNumber(8, 15);
       this.playerHealth -= attackValue;
-      console.log(this.monsterHealth);
     },
     specialAttack() {
       const attackValue = randomNumber(10, 20);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
+      this.currentRound += 1;
+      console.log(this.currentRound);
     }
   }
 });
