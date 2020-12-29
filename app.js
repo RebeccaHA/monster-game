8114a0bb7b990506cbbcf1c8aa7bdbd6edfc326a
RebeccaHA <rebeccahunt034@gmail.com>
2020-12-29 19:43:32 +0000

    added attack monster method, which calculates a number between max and min and takes it off monster health

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
   data() {
     return {
@@ -5,5 +9,18 @@ const app = Vue.createApp({
       playerHealth: 100
     };
   },
-  methods: {}
+  methods: {
+    attackMonster() {
+      const attackValue = randomNumber(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = randomNumber(8, 15);
+      this.playerHealth -= attackValue;
+      console.log(this.monsterHealth);
+    }
+  }
 });
+
+app.mount("#game");
