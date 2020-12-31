1a102ade5dc2750c1d42e9737059c4ac4c8be504
RebeccaHA <rebeccahunt034@gmail.com>
2020-12-31 18:31:25 +0000

    reset the log array when starting game

--- app.js
+++ app.js
@@ -49,8 +49,9 @@ const app = Vue.createApp({
     startGame() {
       (this.winner = null),
         (this.playerHealth = 100),
-        (this.monsterHealth = 100);
-      this.currentRound = 0;
+        (this.monsterHealth = 100),
+        (this.currentRound = 0),
+        (this.battleTexts = []);
     },
     attackMonster() {
       const attackValue = randomNumber(5, 12);
